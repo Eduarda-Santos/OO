157f70d699491dd4fa0e69868b7866c5c4c00903
Eduarda-Santos <eduardas@gmail.com>
2020-02-28 10:06:09 -0300

    inicio

---
+++ src/tads/oo/Main.java
@@ -0,0 +1,20 @@
+package tads.oo;
+
+public class Main {
+
+    public static void metodo1 (int x){
+        int y = 0;
+
+        y = x + 1;
+        System.out.println("Valor de y ="+y);
+    }
+
+
+    public static void main(String[] args) {
+	// write your code here
+
+        System.out.println("hello world");
+
+        metodo1(10);
+    }
+}
