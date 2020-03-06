d5873a8c4514d1eef0f0255d6beb81b1bfc61ab2
Caio Lemos <caio.969@outlook.com>
2020-03-06 10:49:23 -0300

    Atualizaçao0303

--- ContaBancaria.java
+++ ContaBancaria.java
@@ -3,42 +3,22 @@ public class ContaBancaria{
     public int agencia;
     public double saldo;
     public String cliente;
-    Scanner scan = new.Scanner();
 
     public static void sacar(String[] args){
         double saque;
-        
-        System.out.println("Quantidade que deseja sacar:");
-        saque = scan.nextLine;
+    
         saldo = saldo - saque;
 
     }
-
     public static void depositar(String[] args){
         double = depositar;
 
-        System.out.println("Quantida que deseja depositar:");
-        depositar = scan.nextLine;
         saldo = saldo + depositar;
     }
-
     public String toString(){
         String s;
 
         s = "Agencia: "+agencia"Saldo: "+saldo"Cliente: "+cliente;
         return s;
-    }
-}
-public class Main{
-    public static void (String[] args){
-
-        String.out.println("Digite sua agência:");
-        agencia = scan.nextLine;
-        String.out.println("Digite seu saldo:");
-        saldo = scan.nextLine;
-        String.out.println("Digite seu nome:");
-        cliente = scan.nextLine;
-
-        
     }
 }
