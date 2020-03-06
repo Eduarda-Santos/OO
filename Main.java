0230862fe159d0e020b5557b703d6557a564aac5
Caio Lemos <caio.969@outlook.com>
2020-03-06 10:49:47 -0300

    Atualizacao0303

---
+++ Main.java
@@ -0,0 +1,23 @@
+public class Main{
+    public static void main(String[] args){
+
+        //declaraçao
+        ContaBancaria conta1 = null;
+
+        //instanciando
+        conta1 = new ContaBancaria();
+
+        //estado do objeto
+        conta1.agencia = 123;
+        conta1.saldo = 100;
+        conta1.cliente = maria;
+
+
+        System.out.println("Quantidade que deseja sacar:");
+        //chamada de sistema
+
+        System.out.println("Quantida que deseja depositar:");
+        //chamada
+
+    }
+}
